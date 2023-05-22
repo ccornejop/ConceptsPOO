4711f103bb15489c67411e56dc400df404312780
Cristian Cornejo <cristian.cornejo@cruzblanca.cl>
2023-05-22 17:07:21 -0400

    Employee, SalaryEmployee, CommissionEmployee Finish (clase 2)

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -4,17 +4,43 @@ using ConceptsPOO;
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
 
+Employee employee1 = new SaladyEmployee()
+{ 
+    Id = 1010, 
+    FirstName = "Sandra", 
+    LastName = "Morales", 
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.45M
+};
+Console.WriteLine(employee1);
+
+
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+};
+Console.WriteLine(employee2);
+
 //Date date1 = new Date(2022,2,11);
 //Console.WriteLine(date1.ToString());
 
-try
-{
-	Console.WriteLine(new Date(2024, 2, 29));
-	Console.WriteLine(new Date(1974, 9, 23));
-	Console.WriteLine(new Date(1985, 11, 31));
-}
-catch (Exception error)
-{
+//try
+//{
+//	Console.WriteLine(new Date(2024, 2, 29));
+//	Console.WriteLine(new Date(1974, 9, 23));
+//	Console.WriteLine(new Date(1985, 11, 31));
+//}
+//catch (Exception error)
+//{
 
-    Console.WriteLine(error.Message);
-}
+//    Console.WriteLine(error.Message);
+//}
---
+++ ConceptsPOO/ConceptsPOO/SaladyEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class SaladyEmployee : Employee
+    {
+        public decimal Salary { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tValue to pay.........: {$"{GetValueToPay():C2}", 18}";
+        }
+    }
+}
