940564b021ccfb336acf978687971c2b508f1693
Cristian Cornejo <cristian.cornejo@cruzblanca.cl>
2023-05-30 18:26:13 -0400

    Finish POO Concepts

---
+++ ConceptsPOO/ConceptsPOO/HourlyEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class HourlyEmployee : Employee
+    {
+        public float Hours { get; set; }
+
+        public decimal HourValue { get; set; }  
+        public override decimal GetValueToPay()
+        {
+            return (decimal)Hours * HourValue;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tHours................: {$"{Hours:N2}",18}" +
+                $"\n\tHour Value...........: {$"{HourValue:C2}",18}" +
+                $"\n\tValue to pay.........: {$"{GetValueToPay():C2}",18}";
+        }
+    }
+}
